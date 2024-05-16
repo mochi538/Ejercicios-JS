5564149fc0563cf316015b24c42acbcf625fc365
Jerson <jerson014.jep@gmail.com>
2024-05-16 12:52:52 -0500

    segundo commit

---
+++ Nueva carpeta/01Exposición/js/validacion.js
@@ -0,0 +1,8 @@
+let nombre = document.querySelector("#nombre");
+let correo = document.querySelector("#correo");
+let contraseña = document.querySelector("#contraseña");
+
+function validardatos(){
+    if(nombre.value == "" || correo.value == "" || contraseña.value == ""){
+}
+}
