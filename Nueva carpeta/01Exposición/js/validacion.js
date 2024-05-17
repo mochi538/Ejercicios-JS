912495eee870a01ccb896a6a038e2812cda03129
mochi538 <mariar53804@gmail.com>
2024-05-17 13:14:31 -0500

    Commit del formulario

--- Nueva carpeta/01Exposición/js/validacion.js
+++ Nueva carpeta/01Exposición/js/validacion.js
@@ -1,8 +1,40 @@
 let nombre = document.querySelector("#nombre");
 let correo = document.querySelector("#correo");
-let contraseña = document.querySelector("#contraseña");
+let pass = document.getElementById("password");
+let error = document.querySelector("#error");
+let form = document.getElementById("form")
+let mensaje_error = document.createElement("p")
 
-function validardatos(){
-    if(nombre.value == "" || correo.value == "" || contraseña.value == ""){
-}
-}
+form.addEventListener("submit",e=>{
+    e.preventDefault()
+    error.appendChild(mensaje_error)
+
+    let email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    /* Validación nombre */
+    if(nombre.value.length <4){
+        mensaje_error.textContent="Nombre inválido"
+        nombre.style.border="1px solid red"
+    } else{
+        mensaje_error.textContent=""
+        nombre.style.border="none"
+    }
+    
+    /* Validación correo */
+    if(!email.test(correo.value)){
+        mensaje_error.innerText="Correo inválida"
+        correo.style.border="1px solid red"
+    } else{
+        mensaje_error.innerText=""
+        correo.style.border="none"
+    }
+    
+    /* Validación contraseña */
+    if(pass.value.length < 8){
+        alert("Contraseña inválida")
+        pass.style.border="1px solid red"
+    } else{
+        mensaje_error.innerText=""
+        pass.style.border="none"
+    } 
+    
+})
